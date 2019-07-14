23b776fad7a034de66c2dbccf2856b398b8d749a
Patryk Stanek <p.stanek3@gmail.com>
2019-07-14 21:10:29 +0200

    calculateDistancePoints finished

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,32 @@
 const calculateDistancePoints = (distance, hillSize, kPoints) => {
+    /*
+        skocznia normalna 85-109m
+        skocznia duza 110-184m
+        skocznia mamucia 185m+
+    */
+   
+    let start;
+    let multipler;
+    let score;
 
+    if (hillSize >= 85 && hillSize <= 184) {
+        start = 60
+    } else if (hillSize > 184) {
+        start = 120;
+    };
+
+    if (hillSize >= 85 && hillSize <= 109) {
+        multipler = 2;
+    } else if (hillSize >= 110 && hillSize <= 184) {
+        multipler = 1.8;
+    } else if (hillSize >= 185) {
+        multipler = 1.2;
+    };
+
+    score = start + ((distance - kPoints) * multipler);
+    return console.log(score);
 };
+
+calculateDistancePoints(227, 225, 200);
 
 module.exports = calculateDistancePoints;
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,6 +1,8 @@
 const calculateStylePoints = (...args) => {
     let styleNotes = [];
 
+    const BreakException = {};
+
     args.forEach(arg => {
         if (arg < 0 || arg > 20 || (arg % 0.5 != 0)) {
             return console.log('Incorrect style points!');
@@ -9,22 +11,17 @@ const calculateStylePoints = (...args) => {
         }
     });
 
-    let minNote = Math.min(...styleNotes);
-    let maxNote = Math.max(...styleNotes);
-
-    const minNoteFilter = (value) => {
-        return value > minNote;
-    }
-
-    const maxNoteFilter = (value) => {
-        return value < maxNote;
-    }
-
-    let filteredNotes = (styleNotes.filter(minNoteFilter)).filter(maxNoteFilter);
+    styleNotes.sort((a,b) => a-b);
+    styleNotes.shift();
+    styleNotes.pop();
 
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-    return filteredNotes.reduce(reducer);
+    const calculatedPoints = styleNotes.reduce(reducer);
+
+    return calculatedPoints;
 };
+
+// calculateStylePoints(16, 17, 16.5, 16, 18.1);
 
 module.exports = calculateStylePoints;
