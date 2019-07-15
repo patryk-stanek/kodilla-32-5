17fe25846c6eb7137e7564e84ddc38d8b3e6506d
Patryk Stanek <p.stanek3@gmail.com>
2019-07-15 17:43:04 +0200

    expanded stylePoints test

--- calculateStylePoints.test.js
+++ calculateStylePoints.test.js
@@ -17,6 +17,27 @@ describe('calculateStylePoints', () => {
 
             assert.equal(actual, exptected);
         });
+        it('should return 0 if value is too high', () => {
+            const actual = calculateStylePoints([21, 17, 16.5, 16, 18]);
+
+            const exptected = 49.5;
+
+            assert.equal(actual, exptected);
+        });
+        it('should return 0 if value is too low', () => {
+            const actual = calculateStylePoints([0, 17, 16.5, 16, 18]);
+
+            const exptected = 49.5;
+
+            assert.equal(actual, exptected);
+        });
+        it('should return 0 if value wrong (for example 15.4)', () => {
+            const actual = calculateStylePoints([15.4, 17, 16.5, 16, 18]);
+
+            const exptected = 49.5;
+
+            assert.equal(actual, exptected);
+        });
         it('should convert string to number if possible', () => {
             const actual = calculateStylePoints(['12', 17, 16.5, 16, 18]);
 
