cddcf27e5c40a44febf4400bea689eafcb4e1cd2
Patryk Stanek <p.stanek3@gmail.com>
2019-07-15 17:07:21 +0200

    calculateDistancePoints test finished

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -3,11 +3,46 @@ const calculateDistancePoints = (distance, hillSize, kPoints) => {
     let sizeMultipler;
     let totalDistancePoints;
 
+    //-------------------------------------hillSize conditions
+    if (hillSize < 0) {
+        hillSize = 0;
+    } else if (typeof hillSize != 'number') {
+        hillSize = Number(hillSize);
+        
+        if (isNaN(hillSize) == true) {
+            hillSize = 0;
+        }
+    }
+
+    //-------------------------------------distance conditions
+    if (distance < 0) {
+        distance = 0;
+    } else if (typeof distance != 'number') {
+        distance = Number(distance);
+
+        if (isNaN(distance) == true) {
+            distance = 0;
+        };
+    };
+
+    //-------------------------------------kPoints conditions
+    if (kPoints < 0) {
+        kPoints = 0;
+    } else if (typeof kPoints != 'number') {
+        kPoints = Number(kPoints);
+
+        if (isNaN(kPoints) == true) {
+            kPoints = 0;
+        };
+    };
+
     //setting start points
     if (hillSize >= 85 && hillSize <= 184) {
         startPoints = 60
     } else if (hillSize > 184) {
         startPoints = 120;
+    } else {
+        startPoints = 60;//condition for wrong hillSize
     };
 
     //setting multipler
@@ -17,14 +52,19 @@ const calculateDistancePoints = (distance, hillSize, kPoints) => {
         sizeMultipler = 1.8;
     } else if (hillSize >= 185) {
         sizeMultipler = 1.2;
+    } else {
+        sizeMultipler = 1;//condition for wrong hillSize
     };
 
     //calculate total distance points
     totalDistancePoints = startPoints + ((distance - kPoints) * sizeMultipler);
-    console.log(totalDistancePoints);
+
+    if (totalDistancePoints < 0) {
+        totalDistancePoints = 0;
+    };
+
+    // console.log(totalDistancePoints);
     return totalDistancePoints;
 };
-
-// calculateDistancePoints(227, 225, 200);
 
 module.exports = calculateDistancePoints;
--- calculateDistancePoints.test.js
+++ calculateDistancePoints.test.js
@@ -1,2 +1,83 @@
-const asser = require('assert');
+const assert = require('assert');
 const calculateDistancePoints = require('./calculateDistancePoints');
+
+describe('calculateDistancePoints', () => {
+    describe('totalDistancePoints', () => {
+        it('should return 0 if totalScore is negative number', () => {
+            const actual = calculateDistancePoints(0, 225, 200);
+
+            const exptected = 0;
+
+            assert.equal(actual, exptected);
+        })
+    });
+    describe('distance', () => {
+        it('should return 0 if value is NaN', () => {
+            const actual = calculateDistancePoints('string', 225, 200);
+
+            const exptected = 0;
+
+            assert.equal(actual, exptected);
+        });
+        it('should return 0 if value is incorrect', () => {
+            const actual = calculateDistancePoints(-1, 225, 200);
+
+            const exptected = 0;
+
+            assert.equal(actual, exptected);
+        });
+        it('should convert string to number if possible', () => {
+            const actual = calculateDistancePoints('227', 225, 200);
+
+            const exptected = 152.4;
+
+            assert.equal(actual, exptected);
+        });
+    });
+    describe('hillSize', () => {
+        it('should return 0 if value is NaN', () => {
+            const actual = calculateDistancePoints(227, 'string', 200);
+
+            const exptected = 87;
+
+            assert.equal(actual, exptected);
+        });
+        it('should return 0 if value is incorrect', () => {
+            const actual = calculateDistancePoints(227, -1, 200);
+
+            const exptected = 87;
+
+            assert.equal(actual, exptected);
+        });
+        it('should convert string to number if possible', () => {
+            const actual = calculateDistancePoints(227, '225', 200);
+
+            const exptected = 152.4;
+
+            assert.equal(actual, exptected);
+        });
+    });
+    describe('kPoints', () => {
+        it('should return 0 if value is NaN', () => {
+            const actual = calculateDistancePoints(227, 225, 'string');
+
+            const exptected = 392.4;
+
+            assert.equal(actual, exptected);
+        });
+        it('should return 0 if value is incorrect', () => {
+            const actual = calculateDistancePoints(227, 225, -1);
+
+            const exptected = 392.4;
+
+            assert.equal(actual, exptected);
+        });
+        it('should convert string to number if possible', () => {
+            const actual = calculateDistancePoints(227, 225, "200");
+
+            const exptected = 152.4;
+
+            assert.equal(actual, exptected);
+        });
+    });
+});
